f258e7498fb5a52b93c75981d2890cbeac12d792
Vaturia <kkorzun19@gmail.com>
2025-06-26 15:05:13 +0300

    change list.c

--- list.c
+++ list.c
@@ -11,7 +11,7 @@ List* initializationList(void)
 int8_t appendToBeginItem(List *list, TYPE_LIST data)
 {
     if(!list) {return -1;};
-    Node *newNode = calloc(1, sizeof(Node));
+    Node *newNode = (Node*)calloc(1, sizeof(Node));
     if(!newNode) {return -1;};
     
     newNode->data = data;
@@ -24,7 +24,7 @@ int8_t appendToEndItem(List *list, TYPE_LIST data)
 {
     if(!list){return -1;};
 
-    Node *newNode = calloc(1, sizeof(Node));
+    Node *newNode = (Node*)calloc(1, sizeof(Node));
     if(!newNode){return -1;};
     newNode->data = data;
     if(!list->_head)
@@ -71,7 +71,7 @@ int8_t insertItem(List *list, TYPE_LIST data, int32_t num_node, char position)
     int32_t i = 0;
     Node *buf = list->_head, *newNode = 0x00;
 
-    newNode = calloc(1, sizeof(Node));
+    newNode = (Node*)calloc(1, sizeof(Node));
 
     if(!newNode){return -1;};
     for(; (i != num_node - 1) && buf != NULL; buf = buf->_next_node, ++i);
@@ -97,47 +97,4 @@ int8_t insertItem(List *list, TYPE_LIST data, int32_t num_node, char position)
 
     return 0;
 
-}
-int main()
-{
-    List *list = initializationList();
-    for(int8_t i = 0; i < 10; ++i)
-    {
-        appendToEndItem(list, i);
-    }
-    int i = 0;
-    for(Node *item = list->_head; item != 0 ;++i, item = item->_next_node)
-    {
-        
-        printf("Узел номер:%d; Адрес узла:%u; Значение узла:%d Сл. узел:%u;\n", i, item, item->data, item->_next_node);
-
-        
-    }
-    insertItem(list, 37, 7, 'a');
-    insertItem(list, 74, 9, 'b');
-
-    appendToBeginItem(list, 66);
-    i = 0;
-    printf("\n");
-    removeFromEndItem(list);
-    appendToEndItem(list, 77);
-    for(Node *item = list->_head; item != 0 ;++i, item = item->_next_node)
-    {
-        
-        printf("Узел номер:%d; Адрес узла:%u; Значение узла:%d Сл. узел:%u;\n", i, item, item->data, item->_next_node);
-
-        
-    }
-    printf("%u , %u\n", list->_head, list->_tail);
-
-    Node* node_to_find = findItem(list, 10);
-
-    if(node_to_find)
-    {
-        printf("Обнаружен элемент по запросу; Адрес:%u; Значение:%d;\n",node_to_find,node_to_find->data);
-    }
-    else
-    {
-        printf("Элемент не обнаружен\n");
-    }
-}
+};
